30722f5f18b7ac166d73b92cbaed61d2704b4cb9
jblock3 <block.jamie123@gmail.com>
2017-03-30 00:15:50 -0400

    committed

--- jblock3_Assign4.py
+++ jblock3_Assign4.py
@@ -1,3 +1,9 @@
 ##
 # This program was written by Jamie Block (Student #: 250777666)
 #
+
+# Task 1: Implementation of Country class
+
+class Country :
+    def __init__(self, name, pop, area, continent) :
+        self._name = ''
