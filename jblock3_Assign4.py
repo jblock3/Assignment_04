5499abda96653d4b459eed8f5624f15571d9cd7c
jblock3 <block.jamie123@gmail.com>
2017-03-30 00:51:33 -0400

    tested methods

--- jblock3_Assign4.py
+++ jblock3_Assign4.py
@@ -6,4 +6,30 @@
 
 class Country :
     def __init__(self, name, pop, area, continent) :
-        self._name = ''
+        self._name = name
+        self._population = pop
+        self._area = area
+        self._continent = continent
+    def __repr__(self) :
+        print(self._name, 'is in', self._continent, 'with a population density of', self._population / self._area)
+    def setPopulation(self, pop) :
+        self._population = pop
+    def getName(self) :
+        return self._name
+    def getArea(self) :
+        return self._area
+    def getPopulation(self) :
+        return self._population
+    def getContinent(self) :
+        return self._continent
+    def getPopDensity(self) :
+        return self._population / self._area
+
+c1 = Country('China', 1000000000, 9.597, 'Asia')
+print(c1.setPopulation(1000000000))
+print(c1.getName())
+print(c1.getArea())
+print(c1.getPopulation())
+print(c1.getContinent())
+print(c1.getPopDensity())
+c1.__repr__()
