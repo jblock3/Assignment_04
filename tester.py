e0f93c2a240ffa60cbeea6f3a770709860fc5a9a
jblock3 <block.jamie123@gmail.com>
2017-04-03 02:38:23 -0400

    ...

--- tester.py
+++ tester.py
@@ -1,18 +1,25 @@
 class CountryCatalogue:
-    def __init__(self, catalogue = dict(), dictionary = dict()):
-        self._catalogue = catalogue
-        self._cDictionary = dictionary
-        tempCountry = ''
-        tempContinent = ''
-        continentFile = open('continent.txt', 'r')
-        continentLines = continentFile.readlines()[1:]
-        for line in continentFile:
-            splitLine = line.split(',')
-            splitLine[0] = tempCountry
-            splitLine[1] = tempContinent
-            self._cDictionary[tempCountry] = tempContinent
-    def __repr__(self) :
-        print(self._cDictionary)
+    def __init__(self, filename):
+        self._catalogue = {}
+        self._cDictionary = {}
 
-c1 = CountryCatalogue()
+        continentFile = open('continent.txt', 'r')
+        continentFile.readline()
+        for line in continentFile :
+            stripLine = line.rstrip('\n')
+            splitLine = stripLine.split(',')
+            self._cDictionary[splitLine[0]] = splitLine[1]
+
+        userFile = open('data.txt', 'r')
+        userFile.readline()
+        for line2 in userFile :
+            stripLine2 = line2.rstrip('\n')
+            splitLine2 = stripLine2.split('|')
+            self._catalogue[splitLine2[0]] = [splitLine2[1], splitLine2[2]]
+        userFile.close()
+    def __repr__(self):
+        print(self._cDictionary)
+        print(self._catalogue)
+
+c1 = CountryCatalogue('data.txt')
 c1.__repr__()
