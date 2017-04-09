8f4c981d7ee981aa3d79f5c7244ec3d7a77e7d9d
jblock3 <block.jamie123@gmail.com>
2017-04-09 13:46:45 -0400

    ...

--- countryGalore.py
+++ countryGalore.py
@@ -110,12 +110,18 @@ class CountryCatalogue:
         print(largest)
 
     def findCountryWithSmallestArea(self) :
-        smallest = 40000000000000000.0    # Just picked an arbitrarily large number to compare to
+        smallest = 40000000000000000.0    # Just picked an arbitrarily large number to compare to other values
         for key in self._catalogue :
             if self._catalogue[key[2]] < smallest :
                 smallest = self._catalogue[key[2]]
         print(smallest)
 
     def findMostPopulousContinent(self) :
-
+        largest = 0
+        for key in self._catalogue :
+            if self._catalogue[key[1]] > largest :
+                largest = self._catalogue[key[1]]
+                country = key
+        continent = self._cDictionary[country]
+        print(continent)
     def filterCountriesByPopDensity(self) :
